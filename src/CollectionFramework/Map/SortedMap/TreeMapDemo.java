6c59f17006d1fbed4e1b995aef40efcdc35dd029
Ritankar Jana <ritankar.jana.official@gmail.com>
2025-04-19 02:36:21 +0530

    Update TreeMapDemo.java

--- src/CollectionFramework/Map/SortedMap/TreeMapDemo.java
+++ src/CollectionFramework/Map/SortedMap/TreeMapDemo.java
@@ -5,8 +5,16 @@ import java.util.TreeMap;
 
 public class TreeMapDemo {
     public static void main(String[] args) {
-        TreeMap m=new TreeMap(Comparator.nullsFirst(String::compareTo));
-        m.put(null,"Durga");
+//        TreeMap m=new TreeMap(Comparator.nullsFirst(String::compareTo));
+//        m.put(null,"Durga");
+//        System.out.println(m);
+
+        TreeMap m=new TreeMap();
+        m.put(100,"Durga");
+        m.put(200,"Hari Krishna");
+        m.put(300,"Tarak");
+        m.put(400,"Bala");
+        //m.put("AAA","Swami"); ----> RE:-  ClassCastException
         System.out.println(m);
 
     }
