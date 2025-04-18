ff122c87326a5572c09e2324a905dcf133f71937
Ritankar Jana <ritankar.jana.official@gmail.com>
2025-04-19 02:41:28 +0530

    Update TreeMapDemo.java

--- src/CollectionFramework/Map/SortedMap/TreeMapDemo.java
+++ src/CollectionFramework/Map/SortedMap/TreeMapDemo.java
@@ -9,7 +9,10 @@ public class TreeMapDemo {
 //        m.put(null,"Durga");
 //        System.out.println(m);
 
-        TreeMap m=new TreeMap();
+        //TreeMap m=new TreeMap();---> DNSO (Default Natural Sorting Order)
+        // Output:--->  {100=Durga, 200=Hari Krishna, 300=Tarak, 400=Bala}
+        TreeMap m=new TreeMap(new MyComparator());// ---> customized Sorting
+        //Output :- {400=Bala, 300=Tarak, 200=Hari Krishna, 100=Durga}
         m.put(100,"Durga");
         m.put(200,"Hari Krishna");
         m.put(300,"Tarak");
@@ -17,5 +20,14 @@ public class TreeMapDemo {
         //m.put("AAA","Swami"); ----> RE:-  ClassCastException
         System.out.println(m);
 
+    }
+    static class MyComparator implements Comparator
+    {
+        @Override
+        public int compare(Object o1, Object o2) {
+            String s1=o1.toString();
+            String s2=o2.toString();
+            return s2.compareTo(s1);
+        }
     }
 }
