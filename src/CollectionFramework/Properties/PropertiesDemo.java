34ac10a916d6733dc5bd6c6450b84579624a327b
Ritankar Jana <ritankar.jana.official@gmail.com>
2025-04-19 08:48:12 +0530

    added Properties class

---
+++ src/CollectionFramework/Properties/PropertiesDemo.java
@@ -0,0 +1,21 @@
+package CollectionFramework.Properties;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Properties;
+
+public class PropertiesDemo {
+    public static void main(String[] args)throws Exception {
+        Properties p=new Properties();
+        FileInputStream fis=new FileInputStream("E:\\OOPS\\OOPs\\src\\CollectionFramework\\Properties\\abc.properties");
+        p.load(fis);
+        System.out.println(p);
+
+        String s=p.getProperty("name");
+        System.out.println(s);
+        p.setProperty("new-name","Swami Narayan");
+        FileOutputStream fos=new FileOutputStream("E:\\OOPS\\OOPs\\src\\CollectionFramework\\Properties\\abc.properties");
+        p.store(fos,"By Ritankaar Demo Java class");
+        System.out.println(p);
+    }
+}
